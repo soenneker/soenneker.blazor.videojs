b748b83398337bee8d1cbb400408c7d9bf82c5a1
Jake Soenneker <jake@soenneker.com>
2026-04-04 08:28:24 -0500

    quark

--- test/Soenneker.Blazor.Videojs.Demo/Program.cs
+++ test/Soenneker.Blazor.Videojs.Demo/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.JSInterop;
 using Serilog;
 using Serilog.Debugging;
 using Soenneker.Blazor.Videojs.Registrars;
+using Soenneker.Quark.Gen.Lucide.Generated;
 using Soenneker.Serilog.Sinks.Browser.Blazor.Registrars;
 
 namespace Soenneker.Blazor.Videojs.Demo;
@@ -34,6 +35,7 @@ public sealed class Program
 
             builder.Services.AddVideoJsInteropAsScoped();
             builder.Services.AddQuarkSuiteAsScoped();
+            builder.Services.AddLucideIconsAsScoped();
 
             WebAssemblyHost host = builder.Build();
 
